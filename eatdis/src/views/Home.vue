3e9c320230bb3fdcdf1698cab478127dc94044ed
Sasha Menegay <sasha@opus-12.com>
2021-10-19 19:54:00 -0500

    init commit

---
+++ eatdis/src/views/Home.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="home">
+    <marquee>
+      <img src="../../public/undercon3.gif" />
+    </marquee>
+    <img class="logo" src="../../public/eatdiswhite.png" />
+    <br />
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <div class="card">
+            <p>
+              WASSUP Y'ALL WELCOME TO THE EAT DIS SITE :)) . WE GUNNA HAVE A
+              BUNCH FUN STUFF 4 U SOON :))) EACH YOUR VEGGIES LOVE UR FRIENDS A
+              SMALL BUSINESS FUCK RACISTS, TRANSPHOBES, HOMOPHOBES AND DOMESTIC
+              ABUSERS EAT THIS PXSSY BITCH!! A SMALL BUSINESS FUCK RACISTS,
+              TRANSPHOBES, HOMOPHOBES AND DOMESTIC ABUSERS EAT THIS PXSSY
+              BITCH!!!!
+            </p>
+          </div>
+        </div>
+        <div class="col">
+          <img id="construction" src="../../public/under-construction.gif" />
+        </div>
+      </div>
+    </div>
+    <br />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  components: {},
+  methods: {},
+};
+</script>
+
+<style lang="css">
+#app {
+  font-family: sans-serif bold;
+  font-weight: 700;
+}
+#construction {
+  width: 15vw;
+}
+.logo {
+  display: inline-block;
+  width: 50vw;
+  filter: drop-shadow(40px 30px 4px black);
+  padding: 100px 0 100px 0;
+}
+
+p {
+  padding: 20px;
+}
+</style>
