e7e190be5896771c2641233ceddf4a1455eddbf1
Sasha Menegay <sasha@opus-12.com>
2021-10-21 09:56:16 -0500

    more server config

--- eatdis/routes/index.js
+++ eatdis/routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('fs')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
+  fs.readFile('index.html', 'utf-8', (err, content) => {
+    if (err) {
+      console.log('We cannot open "index.html" file.')
+    }
+
+    res.writeHead(200, {
+      'Content-Type': 'text/html; charset=utf-8'
+    })
+
+    res.end(content)
+  })
 });
 
 module.exports = router;
