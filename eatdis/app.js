bcca82af15b9369a232fc0178da4a724089fc707
Sasha Menegay <sasha@opus-12.com>
2021-10-21 09:49:13 -0500

    server config

--- eatdis/app.js
+++ eatdis/app.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "dist")));
 
-app.use("/", indexRouter);
+app.use("/eatdis", indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
